8073e02f7dd6d8fa313b9c1f95afe2b9a18de3c6
Jeffrey Wilcke <stygeo@gmail.com>
2012-11-21 20:21:11 +0100

    Initial Commit

---
+++ pinch_test/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  pinch_test
+//
+//  Created by Jeffrey Wilcke on 11/21/12.
+//  Copyright Indie Pandas 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
